7d27a57d71c507b8c41954b3d780217878c14beb
Julien Palmas <julien.palmas@gmail.com>
2017-11-21 10:41:12 +0100

    setup janrain client id for staging as well

--- app/components/miaa-widget/component.js
+++ app/components/miaa-widget/component.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
+const {computed} = Ember;
+
 export default Ember.Component.extend({
+
+  janrainClientId: computed('', function () {
+    if (window.location.hostname === 'lvh.me') { return 'hrf7zvyde7ms6dph9dd2vr24jqxvxcks';}
+    if (window.location.hostname === 'mon-rdv-pharma-ui.herokuapp.com') { return 'x2cdvqk2nu8664kfu6b4quhzhre9gf3e';}
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    window.miaa_onWidgetLoaded = () => {
+      janrain.settings.capture.clientId = this.get('janrainClientId');
+    }
+  }
+
 });
