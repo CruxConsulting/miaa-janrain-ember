426c6ff527f2c390764ee60d19ce8bd072595920
Julien Palmas <julien.palmas@gmail.com>
2017-11-21 11:03:22 +0100

    move miaa hook functions to the miaa-widget

--- app/components/miaa-widget/component.js
+++ app/components/miaa-widget/component.js
@@ -15,6 +15,54 @@ export default Ember.Component.extend({
     window.miaa_onWidgetLoaded = () => {
       janrain.settings.capture.clientId = this.get('janrainClientId');
     }
+
+    window.miaa_onCaptureLoginSuccess = function(result) {
+        console.log("miaa_onCaptureLoginSuccess");
+        document.getElementById("captureSignInLink").style.display = 'none';
+        document.getElementById("captureRegisterLink").style.display = 'none';
+        document.getElementById("captureSignOutLink").style.display = '';
+        document.getElementById("captureProfileLink").style.display = '';
+    }
+
+    window.miaa_onCaptureSessionFound = function(result) {
+        console.log("miaa_onCaptureSessionFound");
+        document.getElementById("captureSignInLink").style.display = 'none';
+        document.getElementById("captureRegisterLink").style.display = 'none';
+        document.getElementById("captureSignOutLink").style.display = '';
+        document.getElementById("captureProfileLink").style.display = '';
+    }
+
+    window.miaa_onCaptureSessionNotFound = function(){
+        document.getElementById("captureSignInLink").style.display = '';
+        document.getElementById("captureRegisterLink").style.display = '';
+        document.getElementById("captureSignOutLink").style.display = 'none';
+        document.getElementById("captureProfileLink").style.display = 'none';
+    }
+
+    window.miaa_onCaptureRegistrationSuccess = function(result){
+        console.log("miaa_onCaptureRegistrationSuccess");
+    }
+
+    window.miaa_onCaptureSessionEnded = function(result) {
+        console.log("miaa_onCaptureSessionEnded");
+        miaa_onCaptureSessionNotFound();
+    }
+
+    window.miaa_onSecondScreenSaved = function(){
+        console.log("miaa_onSecondScreenSaved");
+    }
+  },
+
+  actions: {
+    signMeIn() {
+      document.getElementById('miaaNonModal').style.display = '';
+      janrain.capture.ui.renderScreen('signIn');
+    },
+
+    register() {
+      document.getElementById('miaaNonModal').style.display = '';
+      janrain.capture.ui.renderScreen('traditionalRegistration');
+    }
   }
 
 });
